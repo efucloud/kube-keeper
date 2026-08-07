678dc3dab22af627e76386827dc9a1a24bd154ac
cloudy <272685110@qq.com>
2026-08-07 10:57:46 +0800

    feat: update

--- web/src/services/mcp_server.api.ts
+++ web/src/services/mcp_server.api.ts
@@ -4,7 +4,7 @@ import { request } from '@umijs/max';
 //
 //
 //请求方法: POST
-//请求地址: /api/stream1/cluster/{cluster}/01kzd2dedjb6126bf9zvafqh4v/check
+//请求地址: /api/stream1/cluster/{cluster}/01kzd2f28s24g2z4zzzrthn7js/check
 //参数名: cluster 参数类型: string 参数位置: path 是否必须: true  参数说明: 集群编码
 export async function posthandleRequest(
   params: {
@@ -12,7 +12,7 @@ export async function posthandleRequest(
   },
   options?: { [key: string]: any }) {
   const { cluster, ...rest } = params;
-  return request(`/api/stream1/cluster/${cluster}/01kzd2dedjb6126bf9zvafqh4v/check`, {
+  return request(`/api/stream1/cluster/${cluster}/01kzd2f28s24g2z4zzzrthn7js/check`, {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
